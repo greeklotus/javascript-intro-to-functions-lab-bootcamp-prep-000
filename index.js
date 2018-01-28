de4a8655edb0989f8a482950b65e667e54fe11bc
Jonathan Juker <juker0274@gmail.com>
2018-01-28 20:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ function shout(string) {
   return string.toUpperCase() }
 function whisper(string) {
   return string.toLowerCase() }
-function logShout(string) {
-  console.log('its one argument')
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO');
+  });
+});
+function logShout(string ){
+  console.log();
+   console.log(string.toUpperCase());
 }
-call ('HELLO')
+function logWhisper(string){
+  console.log();
+  console.log(string.toLowerCase());
+}
+  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) 
+    return "I can't hear you!";
+  }
+  var sayHiToGrandma = uppercase;
+    uppercase.toUpperCase() === 
+    uppercase;
+ 
+
+
+
